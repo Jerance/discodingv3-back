a077ee882af48aed1098a1855fbf2b29d1357452
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-11-09 11:32:22 +0100

    added functions to delete and quit server

--- src/modules/servers/servers.services.ts
+++ src/modules/servers/servers.services.ts
@@ -11,7 +11,9 @@ export async function createServer(req: Request) {
         roles: req.body.roles
     })
 
-    await UsersServers.insertOne({ server_id: new ObjectId(server.insertedId), users: [new ObjectId(req.user?._id)] })
+    await UsersServers.insertOne({ server_id: new ObjectId(server.insertedId), users: [{ idUser: new ObjectId(req.user?._id) }] })
+
+    await Users.updateOne({ _id: new ObjectId(req.user?._id) }, { $push: { servers: { idServer: new ObjectId(server.insertedId), banStatus: false, roles: ["owner", "admin"] } } })
 
     if(!server) {
         return { success: false }
@@ -31,13 +33,41 @@ export async function findServerById(id: string) {
 }
 
 export async function joinServer(req: Request) {
-    const join = await UsersServers.updateOne({ server_id: new ObjectId(req.params.idServer) }, { $push: { users: new ObjectId(req.user?._id) }})
+    const join = await UsersServers.updateOne({ server_id: new ObjectId(req.params.idServer) }, { $push: { users: { idUser: new ObjectId(req.user?._id) } }})
 
-    const userUpdate = await Users.updateOne({ _id: new ObjectId(req.user?._id) }, { $push: { servers: new ObjectId(req.params.idServer) } })
+    const userUpdate = await Users.updateOne({ _id: new ObjectId(req.user?._id) }, { $push: { servers: { idServer: new ObjectId(req.params.idServer), banStatus: false, roles: ["member"] } } })
 
     if(!join || !userUpdate) {
         return { success: false }
     }
 
     return { success: true, join }
+}
+
+export async function quitServer(req: Request){
+
+    if(req.user?._id && req.params.idServer){
+        await Users.updateOne({ _id: req.user?._id },{ $pull: { servers: { idServer: new ObjectId(req.params.idServer) } } })
+        await UsersServers.updateOne({ server_id: new ObjectId(req.params.idServer)}, { $pull: { users: { idUser: req.user?._id } } })
+
+        return { success: true }
+    }
+
+    return { success: false }
+}
+
+export async function deleteServer(req: Request){
+
+    if(req.params.idServer) {
+
+        await Users.updateMany({ "servers.idServer": new ObjectId(req.params.idServer) }, { $pull: { servers: { idServer: new ObjectId(req.params.idServer) } } })
+
+        await UsersServers.deleteOne({ server_id: new ObjectId(req.params.idServer) })
+
+        await Servers.deleteOne({ _id: new ObjectId(req.params.idServer) })
+
+        return { success: true }
+    }
+
+    return { success: false }
 }
