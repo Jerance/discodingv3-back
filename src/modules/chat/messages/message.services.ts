e8f547e6a2a30533b5f0eb464a49ec824f82c6fd
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2023-11-10 13:59:53 +0100

    update messages services

--- src/modules/chat/messages/message.services.ts
+++ src/modules/chat/messages/message.services.ts
@@ -12,6 +12,16 @@ export async function getAllMessagesInConversation(conversationId: string): Prom
     }
 }
 
+// export async function getAllMessageBySrcId(srcId): Promise<Messages[] | null> {
+//     try {
+//         const messages = await Message.find({ idSrc: new ObjectId(conversationId) }).toArray();
+//         return messages;
+//     } catch (error) {
+//         console.error("Error fetching messages in conversation:", error);
+//         return null;
+//     }
+// }
+
 export async function sendMessage(newMessage: Messages): Promise<boolean> {
     try {
         newMessage.senderId = new ObjectId(newMessage.senderId);
